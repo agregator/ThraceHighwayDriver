23d01164f54bfa797657db28bb42fbf8a71a8839
agregator <bunovski@uni-sofia.bg>
2015-06-08 18:22:34 +0300

    created structure of class car

---
+++ src/car.py
@@ -0,0 +1,20 @@
+class Car:
+
+    def __init__(self, body):
+        pass
+
+    def update(self):
+        pass
+
+    def increase_speed(self, value):
+        pass
+
+    def decrease_speed(self, value):
+        pass
+
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
