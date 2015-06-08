808b79b5703fd2dcfe0c273c78257086563cb103
agregator <bunovski@uni-sofia.bg>
2015-06-08 19:06:09 +0300

    created structure of class timer

---
+++ src/timer.py
@@ -0,0 +1,15 @@
+class Timer:
+
+    time_to_reach_checkpoint = 0
+
+    def time_left(self):
+        pass
+
+    def start_time(self):
+        pass
+
+    def end_time(self):
+        pass
+
+    def tick(self):
+        pass
